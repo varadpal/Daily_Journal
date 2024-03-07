1342f196b10edde119c36f1785dc6f4d02b30c6b
socketgut <varadpal.1308@gmail.com>
2024-03-08 00:49:37 +0530

    changes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express"; 
 import bodyParser from "body-parser";
+import pg from "pg";
 
 // To make __dirname 
 import { dirname } from "path"; 
@@ -10,6 +11,15 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000; 
 
+const db = new pg.Client({
+    user:"postgres",
+    host:"localhost", 
+    database:"dailyJournal", 
+    password:"varad",
+    port: 5432,
+});
+
+db.connect();
 
 // Initializing Body Parser
 app.use(bodyParser.urlencoded({extended: true}));
@@ -19,10 +29,17 @@ app.use(express.static("public"));
 
 app.get("/", function (req, res) {
     res.render("main");
+
+    const result = db.query("SELECT journal FROM journals WHERE id = 1");
+    console.log(result);
 })
 
 app.post("/submit", function (req, res) {
-    console.log(req.body);
+    let journal = req.body.journal;
+    // console.log(journal);
+    
+    db.query("INSERT INTO journals (journal) VALUES ($1)",[journal,]); 
+    res.redirect("/");
 })
 
 app.listen(process.env.PORT || port, function () {
